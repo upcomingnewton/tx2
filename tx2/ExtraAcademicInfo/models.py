13fc3ec59533d7de2e9eb9a4a44e96a7708db0d7
Jivjot <jivjot@gmail.com>
2012-07-04 16:46:23 +0530

    Added Logs

--- tx2/ExtraAcademicInfo/models.py
+++ tx2/ExtraAcademicInfo/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from tx2.Users.models import User
+from tx2.Security.models import SecurityPermissions
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class ExtraAcademicInfo(models.Model):
@@ -14,3 +17,15 @@ class ExtraAcademicInfoDetails(models.Model):
     Venue=models.CharField(max_length=1000)
     Duration_in_weeks=models.IntegerField()
     ExtraAcadmicInfoId=models.ForeignKey(ExtraAcademicInfo)
+    
+class ExtraAcademicInfoLog(models.Model):
+    # user making changes
+    LogsUser = models.ForeignKey(User)
+    # row id being changed
+    ContentType = models.ForeignKey(ContentType)
+    LogsObject = models.IntegerField()
+    LogsPermission = models.ForeignKey(SecurityPermissions)
+    LogsIP = models.CharField(max_length=20)
+    LogsTimeStamp = models.DateTimeField()
+    LogsDescription = models.CharField(max_length=200)
+    LogsPreviousState = models.CharField(max_length=5000)
