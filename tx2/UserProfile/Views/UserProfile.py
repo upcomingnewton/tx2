e57d33598b6829ef15a3800c8ddfac7321e849b5
nitin chadha <upcomingnewton@gmail.com>
2012-07-30 01:02:04 +0530

    changed template for user profile creation

--- tx2/UserProfile/Views/UserProfile.py
+++ tx2/UserProfile/Views/UserProfile.py
@@ -10,6 +10,7 @@ from tx2.Misc.MIscFunctions1 import AppendMessageList
 from tx2.Users.HelperFunctions.LoginDetails import GetLoginDetails
 from tx2.CONFIG import  SESSION_MESSAGE, LoggerSecurity
 from tx2.UserProfile.BusinessFunctions.UserProfile import UserProfile
+from tx2.UserProfile.models import Degree,Branch,Category
 from tx2.Misc.MIscFunctions1 import is_integer
 import logging
 Logger_User = logging.getLogger(LoggerSecurity)
@@ -82,99 +83,83 @@ def CategoryInsert(HttpRequest):
         print 'y =', y
 
 def StudentDetailsIndex(HttpRequest):
-    return render_to_response("UserProfile/StudentDetails.html",context_instance=RequestContext(HttpRequest))
+    return render_to_response("UserProfile/StudentDetails.html",{'BranchList':Branch.objects.all(),'CategoryList':Category.objects.all(),'DegreeList':Degree.objects.all()},context_instance=RequestContext(HttpRequest))
+    
 def StudentDetailsInsert(HttpRequest):
-    msglist = AppendMessageList(HttpRequest)
-    ip = HttpRequest.META['REMOTE_ADDR']
-    logindetails = GetLoginDetails(HttpRequest)
-    print logindetails
-    if( logindetails["userid"] == -1):
-        msglist.append('Please Login to continue')
-        HttpRequest.session[SESSION_MESSAGE] = msglist
-        return HttpResponseRedirect('/user/login/')
-    try:
-        UserProfileObj=UserProfile()
-        flag=1
-        if "UserId" in HttpRequest.POST:
-            UserId=HttpRequest.POST["UserId"]
-            if is_integer(UserId):
-                UserId=int(UserId)
-            else:
-                msglist.append("Error integer conversion from form for UserID");
-                flag=-1
-        else:
-            msglist.append("Error fetching data from form for RollNo");
-            flag=-1;
-      
-        if "RollNo" in HttpRequest.POST:
-            RollNo=HttpRequest.POST["RollNo"]
-        else:
-            msglist.append("Error fetching data from form for RollNo");
-            flag=-1;
-        
-        if "BranchMajor" in HttpRequest.POST:
-            BranchMajor=HttpRequest.POST["BranchMajor"]
-            if is_integer(BranchMajor):
-                BranchMajor=int(BranchMajor)
-            else:
-                msglist.append("Error integer conversion from form for BranchMajor");
-                flag=-1
-        else:
-            msglist.append("Error fetching data from form for BranchMajor");
-            flag=-1;
-        
-        if "BranchMinor" in HttpRequest.POST:
-            BranchMinor=HttpRequest.POST["BranchMinor"]
-            if is_integer(BranchMinor):
-                BranchMajor=int(BranchMinor)
-            else:
-                msglist.append("Error integer conversion from form for BranchMinor");
-                flag=-1
-        else:
-            msglist.append("Error fetching data from form for BranchMinor");
-            flag=-1;                    
-        
-        if "Degree" in HttpRequest.POST:
-            Degree=HttpRequest.POST["Degree"]
-            if is_integer(Degree):
-                Degree=int(Degree)
-            else:
-                msglist.append("Error integer conversion from form for Degree");
-                flag=-1
-        else:
-            msglist.append("Error fetching data from form for Degree");
-            flag=-1;
-        
-        if "Category" in HttpRequest.POST:
-            Category=HttpRequest.POST["Category"]
-            if is_integer(Category):
-                Category=int(Category)
-            else:
-                msglist.append("Error integer conversion from form for Category");
-                flag=-1
-        else:
-            msglist.append("Error fetching data from form for Category");
-            flag=-1;
-        
-        if "ComputerProficiency" in HttpRequest.POST:
-            ComputerProficiency=HttpRequest.POST["ComputerProficiency"]
-            
-        else:
-            msglist.append("Error fetching data from form for ComputerProficiency");
-            flag=-1;     
-        if flag==-1:
+        msglist = AppendMessageList(HttpRequest)
+        ip = HttpRequest.META['REMOTE_ADDR']
+        logindetails = GetLoginDetails(HttpRequest)
+        if( logindetails["userid"] == -1):
+            msglist.append('Please Login to continue')
             HttpRequest.session[SESSION_MESSAGE] = msglist
-            return render_to_response("UserProfile/Message.html",{'mylist':msglist,})
-        result=UserProfileObj.InsertStudentDetails(UserId, RollNo, BranchMajor, BranchMinor, Degree, Category, ComputerProficiency,logindetails["userid"], ip)
-        msglist.append("result is %s"%result);
-        return render_to_response("UserProfile/Message.html",{'mylist':msglist,})
-    except Exception as inst:
-        print type(inst)     # the exception instance
-        print inst.args      # arguments stored in .args
-        print inst           # __str__ allows args to printed directly
-        x, y = inst.args
-        print 'x =', x
-        print 'y =', y
+            return HttpResponseRedirect('/user/login/')
+        try:
+            UserProfileObj=UserProfile()
+            flag=1
+            UserId=int(UserId)
+            RollNo = -1
+            BranchMajor = -1
+            BranchMinor = -1
+            Degree = -1
+            Category = -1
+            ComputerProficiency = ""
+            if "RollNo" in HttpRequest.POST:
+                RollNo=HttpRequest.POST["RollNo"]
+            else:
+                msglist.append("Error fetching data from form for RollNo");
+                flag=-1;
+            
+            if "BranchMajor" in HttpRequest.POST:
+                BranchMajor= int(HttpRequest.POST["BranchMajor"])
+                if BranchMajor == -1:
+                    msglist.append("Please select value for BranchMajor");
+                    flag=-1;
+            else:
+                msglist.append("Error fetching data from form for BranchMajor");
+                flag=-1;
+            
+            if "BranchMinor" in HttpRequest.POST:
+                BranchMinor= int(HttpRequest.POST["BranchMinor"])
+            else:
+                msglist.append("Error fetching data from form for BranchMinor");
+                flag=-1;                    
+            
+            if "Degree" in HttpRequest.POST:
+                Degree=int(HttpRequest.POST["Degree"])
+                if Degree == -1:
+                    msglist.append("Please select value for Degree");
+                    flag=-1;
+            else:
+                msglist.append("Error fetching data from form for Degree");
+                flag=-1;
+            
+            if "Category" in HttpRequest.POST:
+                Category=int(HttpRequest.POST["Category"])
+                if Category == -1:
+                    msglist.append("Please select value for Category");
+                    flag=-1;
+            else:
+                msglist.append("Error fetching data from form for Category");
+                flag=-1;
+            
+            if "ComputerProficiency" in HttpRequest.POST:
+                ComputerProficiency=HttpRequest.POST["ComputerProficiency"]
+            else:
+                ComputerProficiency = "Not applicable"  
+            if flag==-1:
+                HttpRequest.session[SESSION_MESSAGE] = msglist
+                return HttpResponseRedirect('/message/')
+            result=UserProfileObj.InsertStudentDetails(UserId, RollNo, BranchMajor, BranchMinor, Degree, Category, ComputerProficiency,UserId, ip)
+            msglist.append("result is %s"%result)
+            HttpRequest.session[SESSION_MESSAGE] = msglist
+            return HttpResponseRedirect('/message/')
+        except Exception as inst:
+            print type(inst)     # the exception instance
+            print inst.args      # arguments stored in .args
+            print inst           # __str__ allows args to printed directly
+            x, y = inst.args
+            print 'x =', x
+            print 'y =', y
 
 
 
