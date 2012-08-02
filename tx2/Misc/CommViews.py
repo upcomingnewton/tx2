8a4adcf381900844841c4892d5cafde98364ef10
nitin chadha <upcomingnewton@gmail.com>
2012-08-02 21:27:50 +0530

    added log messages to our redirection

--- tx2/Misc/CommViews.py
+++ tx2/Misc/CommViews.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect,HttpResponse
 from tx2.CONFIG import LoggerUser
 import logging
 LoggerUser = logging.getLogger(LoggerUser)
@@ -7,4 +7,5 @@ LoggerUser = logging.getLogger(LoggerUser)
 def NoticeIndex(noticeid):
     s = 'http://www.tnpuiet.thoughtxplore.com/comm/notice/iframe/' + str(noticeid)
     LoggerUser.debug('redirecting to : %s'%(s) )
-    return HttpResponseRedirect(s)
+    return HttpResponse('thanks')
+    #return HttpResponseRedirect(s)
