d0cedd9fbdc7815823bcfcdec16eca58b24f7c87
Jivjot <jivjot@gmail.com>
2012-08-03 17:37:10 +0530

    execute all sp's in a folder

--- tx2/Misc/MIscFunctions1.py
+++ tx2/Misc/MIscFunctions1.py
@@ -1,13 +1,14 @@
 from tx2.CONFIG import SESSION_MESSAGE
-
+from tx2.DataBaseHelper import DBhelper
+import os
 def AppendMessageList(HttpRequest):
     msglist = []
     try:
-	    if SESSION_MESSAGE in HttpRequest.session.keys():
-		msglist = HttpRequest.session[SESSION_MESSAGE]
-		print '=== message list appended'
+      if SESSION_MESSAGE in HttpRequest.session.keys():
+        msglist = HttpRequest.session[SESSION_MESSAGE]
+      print '=== message list appended'
     except:
-   	pass
+      pass
     print '||msglist||' + str(msglist)
     return msglist
 def is_integer(s):
@@ -15,4 +16,27 @@ def is_integer(s):
         int(s)
         return True
     except :
-        return False
+      print "Hi"
+        
+def executeAllStoredProcedures(path):
+  try:
+    temp=os.listdir(path)
+    print temp
+    for subdir in temp:
+      temp2=os.path.join(path,subdir)
+      if os.path.isfile(temp2):
+        try:
+          
+          f=open(temp2,'r')
+          sql=f.read()
+          DBhelper.CallFunction(sql)
+        except Exception as ex:
+          print ex
+        continue
+      executeAllStoredProcedures(temp2)
+  except Exception as ex:
+    print ex
+  return 0;
+
+if __name__=='__main__':
+  executeAllStoredProcedures(os.path.realpath('../../sp'))
