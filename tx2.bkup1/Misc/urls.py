7e5f23cb93daa7211c78addef95d1ef59822731f
nitin chadha <upcomingnewton@gmail.com>
2012-08-06 21:17:57 +0530

    updating everything alongwith that on server

--- tx2.bkup1/Misc/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('',
-                       # states
-                       url(r'^notice/iframe/(?P<noticeid>\d+)/$','Misc.CommViews.NoticeIndex'),
-                    )
--- tx2.bkup1/settings.py
+++
@@ -1,9 +0,0 @@
-
-import os
-#hostname = os.environ['HOSTNAME']
-#print hostname
-
-#if( hostname == 'linux-4bv4.site' ):
-from conf.nitin_settings import *
-#elif (hostname == 'nathron.thoughtexplore.com'):
-#from conf.labs_nitin_settings import *
