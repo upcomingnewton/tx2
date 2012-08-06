cefdb02146ce9c417c1f18525a773533a4460bfb
nitin chadha <upcomingnewton@gmail.com>
2012-08-07 00:10:22 +0530

    update db for time being

--- tx2/conf/uiet_settings.py
+++ tx2/conf/uiet_settings.py
@@ -13,16 +13,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tnpuiet2013',                      # Or path to database file if using sqlite3.
-        'USER': 'ThoughtXplore',                      # Not used with sqlite3.
-        'PASSWORD': 'Nath@3728',                  # Not used with sqlite3.
-        'HOST': '119.18.58.214',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
--- tx2/settings.py
+++ tx2/settings.py
@@ -5,5 +5,6 @@ import os
 
 #if( hostname == 'linux-4bv4.site' ):
 from conf.nitin_settings import *
+from conf.db_settings import *
 #elif (hostname == 'nathron.thoughtexplore.com'):
 #from conf.labs_nitin_settings import *
