8b0e2efb46bb33fe2212a5310a0df3d28d86204c
nitin chadha <upcomingnewton@gmail.com>
2012-09-15 23:45:14 +0530

    corrected job type -branch post bug

--- tx2/jobs/Views/JobViews.py
+++ tx2/jobs/Views/JobViews.py
@@ -330,6 +330,7 @@ def BranchJobEdit(HttpRequest,CompanyID, JobID):
         isinitial = 'Initial_' + str(branch.id)
         if (isinitial) in HttpRequest.POST:
           isinitial = HttpRequest.POST[isinitial]
+          result = [1,-1]
           if isinitial != "-1":
             # edit
             _id = int(isinitial)
@@ -344,7 +345,7 @@ def BranchJobEdit(HttpRequest,CompanyID, JobID):
               Comments2 = HttpRequest.POST["Remarks_" + str(branch.id)]
               JobTypeID = HttpRequest.POST["JobType_" + str(branch.id)]
               result = BranchJobFunctionsObj.Add(branch.id,JobID,JobTypeID,Comments1,Comments2,int(details['userid']),HttpRequest.META['REMOTE_ADDR'])
-          print str(result) + '==' + branch.BranchName
+          #print str(result) + '==' + branch.BranchName
           if result[0] != 1:
             messages.error(HttpRequest,'ERROR : ' + result[1])
             return HttpResponseRedirect('/message/')
