512f3e91208198deccaecd1b41e6221ad3595efc
nitin chadha <upcomingnewton@gmail.com>
2012-10-25 20:42:43 +0530

    revised gitignore

--- tx2.backup.sept22/Misc/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-
-urlpatterns = patterns('',
-                       # states
-                       url(r'^notice/iframe/(?P<noticeid>\d+)/$','Misc.CommViews.NoticeIndex'),
-                       url(r'^test/','Misc.CommViews.TestIndex')
-                    )
